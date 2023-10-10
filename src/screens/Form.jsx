3a0725a006a726a4594cda6dbcb81b652b0d2e93
hugolhld <hugo.lhernould@gmail.com>
2023-10-10 21:26:20 +0200

    add css animation for skills

--- src/screens/Form.jsx
+++ src/screens/Form.jsx
@@ -25,22 +25,22 @@ const Form = () => {
         <TitleSection title='.Say Hi' />
         <form ref={formRef} onSubmit={sendMail} className='flex flex-col w-4/5'>
           <label className='text-white font-semibold pt-4 pb-2' htmlFor="mail">Votre adresse mail</label>
-          <input className='rounded-lg py-2 px-2 focus:outline-none border-4 border-purple-500' type="email" name="mail" id="mail" />
+          <input className='rounded-lg py-2 px-2 focus:outline-none shadow-xl' type="email" name="mail" id="mail" />
           
           <div className='flex justify-between'>
             <div className=' w-full flex flex-col mr-2'>
               <label className='text-white font-semibold pt-4 pb-2' htmlFor="name">Votre nom</label>
-              <input className=' w-full rounded-lg py-2 px-2 focus:outline-none border-4 border-purple-500' id='name' type="text" />
+              <input className=' w-full rounded-lg py-2 px-2 focus:outline-none shadow-xl' id='name' type="text" />
             </div>
 
             <div className="w-full flex flex-col ml-2">
               <label className='text-white font-semibold pt-4 pb-2' htmlFor="subject">Sujet</label>
-              <input className='w-full rounded-lg py-2 px-2 focus:outline-none border-4 border-purple-500' id='subject' type="text" />
+              <input className='w-full rounded-lg py-2 px-2 focus:outline-none shadow-xl' id='subject' type="text" />
             </div>
           </div>
           
           <label className='text-white font-semibold pt-4 pb-2' htmlFor="message">Votre demande</label>
-          <textarea className='p-2 rounded-lg resize-none border-4 border-purple-500' name="text" id="message" cols="30" rows="10"></textarea>
+          <textarea className='p-2 rounded-lg resize-none shadow-xl' name="text" id="message" cols="30" rows="10"></textarea>
           <div className='flex justify-end'>
             <input type="submit" value="Envoyer" className='bg- text-white font-semibold w-1/3 my-4 rounded-lg py-2 hover:cursor-pointer' style={{background: 'linear-gradient(45.46deg, rgba(187, 21, 41) 0.94%, rgba(143, 29, 197) 52.06%)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}} />
           </div>
--- src/screens/Skills.jsx
+++ src/screens/Skills.jsx
@@ -13,23 +13,104 @@ const Skills = () => {
         {/* <h2 className='text-3xl text-start font-semibold text-white p-16'>.Skills</h2> */}
         <TitleSection title={'.Skills'} />
         <ul className='flex w-4/5  justify-between items-center flex-wrap mx-auto gap-y-4'>
-          <li className='flex justify-center w-1/3' >
-            <img src={reactImg} alt="" className='w-32 h-32 p-4 rounded-full' style={{background: 'linear-gradient(92.46deg, rgba(187, 21, 41) 0.94%, rgba(143, 29, 197) 52.06%)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}/>
+          
+          <li className='flex justify-center w-1/3 group perspective ' >
+            <div className="relative w-32 h-32 p-4 rounded-full transition-all duration-1000 preserve-3d group-hover:my-rotate-y-180" style={{background: 'linear-gradient(92.46deg, rgba(187, 21, 41) 0.94%, rgba(143, 29, 197) 52.06%)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}>
+              <div className="absolute inset-0">
+                <img src={reactImg} alt="" className='w-full h-full p-4' />
+              </div>
+              <div className="absolute inset-0 w-full h-full rounded-full text-white my-rotate-y-180 backface-hidden" style={{background: 'linear-gradient(270.46deg, rgba(187, 21, 41) 0.94%, rgba(143, 29, 197) 52.06%)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}>
+                <a href="" className='w-full h-full flex justify-center items-center font-semibold'>
+                  ReactJS
+                </a>
+              </div>
+            </div>
           </li>
-          <li className='flex justify-center w-1/3' >
+
+
+          {/* <li className='flex justify-center w-1/3' >
             <img src={tailwindImg} alt="" className='w-32 h-32 p-4 rounded-full' style={{background: 'rgba(143, 29, 197)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}/>
+          </li> */}
+
+          <li className='flex justify-center w-1/3 group perspective ' >
+            <div className="relative w-32 h-32 p-4 rounded-full transition-all duration-1000 preserve-3d group-hover:my-rotate-y-180" style={{background: 'rgba(143, 29, 197)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}>
+              <div className="absolute inset-0">
+                <img src={tailwindImg} alt="" className='w-full h-full p-4' />
+              </div>
+              <div className="absolute inset-0 w-full h-full rounded-full text-white my-rotate-y-180 backface-hidden" style={{background: 'rgba(143, 29, 197)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}>
+                <a href="" className='w-full h-full flex justify-center items-center font-semibold'>
+                  ReactJS
+                </a>
+              </div>
+            </div>
           </li>
-          <li className='flex justify-center w-1/3'>
+
+          {/* <li className='flex justify-center w-1/3'>
             <img src={threeImg} alt="" className='w-32 h-32 rounded-full p-4 pl-8' style={{background: 'linear-gradient(-92.46deg, rgba(187, 21, 41) 0.94%, rgba(143, 29, 197) 52.06%)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}/>
+          </li> */}
+
+          <li className='flex justify-center w-1/3 group perspective ' >
+            <div className="relative w-32 h-32 p-4 rounded-full transition-all duration-1000 preserve-3d group-hover:my-rotate-y-180" style={{background: 'linear-gradient(-92.46deg, rgba(187, 21, 41) 0.94%, rgba(143, 29, 197) 52.06%)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}>
+              <div className="absolute inset-0 ">
+                <img src={threeImg} alt="" className='w-full h-full p-4  pl-8' />
+              </div>
+              <div className="absolute inset-0 w-full h-full rounded-full text-white my-rotate-y-180 backface-hidden" style={{background: 'linear-gradient(92.46deg, rgba(187, 21, 41) 0.94%, rgba(143, 29, 197) 52.06%)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}>
+                <a href="" className='w-full h-full flex justify-center items-center font-semibold'>
+                  ReactJS
+                </a>
+              </div>
+            </div>
           </li>
-          <li className='flex justify-center w-1/3' >
+
+          {/* <li className='flex justify-center w-1/3' >
             <img src={rapierImg} alt="" className='w-32 h-32 rounded-full p-4' style={{background: 'linear-gradient(92.46deg, rgba(187, 21, 41) 0.94%, rgba(143, 29, 197) 52.06%)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}/>
+          </li> */}
+
+          <li className='flex justify-center w-1/3 group perspective ' >
+            <div className="relative w-32 h-32 p-4 rounded-full transition-all duration-1000 preserve-3d group-hover:my-rotate-y-180" style={{background: 'linear-gradient(92.46deg, rgba(187, 21, 41) 0.94%, rgba(143, 29, 197) 52.06%)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}>
+              <div className="absolute inset-0">
+                <img src={rapierImg} alt="" className='w-full h-full p-4' />
+              </div>
+              <div className="absolute inset-0 w-full h-full rounded-full text-white my-rotate-y-180 backface-hidden" style={{background: 'linear-gradient(-92.46deg, rgba(187, 21, 41) 0.94%, rgba(143, 29, 197) 52.06%)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}>
+                <a href="" className='w-full h-full flex justify-center items-center font-semibold'>
+                  ReactJS
+                </a>
+              </div>
+            </div>
           </li>
-          <li className='flex justify-center w-1/3'>
+
+          {/* <li className='flex justify-center w-1/3'>
             <img src={firebaseImg} alt="" className='w-32 h-32 rounded-full p-4' style={{background: 'rgba(143, 29, 197)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}/>
+          </li> */}
+
+          <li className='flex justify-center w-1/3 group perspective ' >
+            <div className="relative w-32 h-32 p-4 rounded-full transition-all duration-1000 preserve-3d group-hover:my-rotate-y-180" style={{background: 'rgba(143, 29, 197)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}>
+              <div className="absolute inset-0">
+                <img src={firebaseImg} alt="" className='w-full h-full p-4' />
+              </div>
+              <div className="absolute inset-0 w-full h-full rounded-full text-white my-rotate-y-180 backface-hidden" style={{background: 'rgba(143, 29, 197)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}>
+                <a href="" className='w-full h-full flex justify-center items-center font-semibold'>
+                  ReactJS
+                </a>
+              </div>
+            </div>
           </li>
-          <li className='flex justify-center w-1/3' >
+
+          {/* <li className='flex justify-center w-1/3' >
             <img src={githubImg} alt="" className='w-32 h-32 rounded-full p-4' style={{background: 'linear-gradient(-92.46deg, rgba(187, 21, 41) 0.94%, rgba(143, 29, 197) 52.06%)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}/>
+          </li> */}
+
+          <li className='flex justify-center w-1/3 group perspective ' >
+            <div className="relative w-32 h-32 p-4 rounded-full transition-all duration-1000 preserve-3d group-hover:my-rotate-y-180" style={{background: 'linear-gradient(-92.46deg, rgba(187, 21, 41) 0.94%, rgba(143, 29, 197) 52.06%)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}>
+              <div className="absolute inset-0">
+                <img src={githubImg} alt="" className='w-full h-full p-4' />
+              </div>
+              <div className="absolute inset-0 w-full h-full rounded-full text-white my-rotate-y-180 backface-hidden" style={{background: 'linear-gradient(92.46deg, rgba(187, 21, 41) 0.94%, rgba(143, 29, 197) 52.06%)', boxShadow: '0px 3px 4px 1px rgba(0, 0, 0, 0.25)'}}>
+                <a href="" className='w-full h-full flex justify-center items-center font-semibold'>
+                  ReactJS
+                </a>
+              </div>
+            </div>
           </li>
           
         </ul>
